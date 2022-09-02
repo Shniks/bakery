3bdc7dd06d1cbe8948535647c30a29e073724833
Nikhil Shahi <shniks@gmail.com>
2022-09-02 10:57:00 -0700

    Added simplecov and database cleaner gems.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -63,4 +65,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+DatabaseCleaner.strategy = :truncation
+
+RSpec.configure do |c|
+  c.before(:each) do
+    DatabaseCleaner.clean
+  end
+
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
+  c.include Capybara::DSL
 end
